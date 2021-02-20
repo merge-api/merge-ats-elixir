f3adec9e35d6996561765dacdcfabb43058ab9c6
Dan <dan@merge.dev>
2021-02-20 14:17:27 -0800

    make categories list

--- lib/merge_ats_client/model/account_integration.ex
+++ lib/merge_ats_client/model/account_integration.ex
@@ -18,7 +18,7 @@ defmodule MergeATSClient.Model.AccountIntegration do
 
   @type t :: %__MODULE__{
     :"name" => String.t,
-    :"categories" => String.t | nil,
+    :"categories" => [String.t] | nil,
     :"image" => String.t | nil,
     :"square_image" => String.t | nil,
     :"color" => String.t | nil
