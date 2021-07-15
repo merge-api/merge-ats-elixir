26698c2b5b2644c3fce76caf7480d8100732a3e5
Dan <dan@merge.dev>
2021-07-14 17:17:13 -0700

    update mix.exs for Hex

--- mix.exs
+++ mix.exs
@@ -7,6 +7,10 @@ defmodule MergeATSClient.Mixfile do
      elixir: "~> 1.6",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     name: "MergeATSClient",
+     description: description(),
+     package: package(),
+     source_url: "https://github.com/merge-api/merge-ats-elixir",
      deps: deps()]
   end
 
@@ -30,7 +34,20 @@ defmodule MergeATSClient.Mixfile do
   defp deps do
     [
       {:tesla, "~> 1.2"},
-      {:poison, "~> 3.0"}
+      {:poison, "~> 3.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
+
+  defp description() do
+    "The unified API for building rich integrations with multiple Applicant Tracking System platforms."
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/merge-api/merge-ats-elixir"}
+    ]
+  end
+
 end
